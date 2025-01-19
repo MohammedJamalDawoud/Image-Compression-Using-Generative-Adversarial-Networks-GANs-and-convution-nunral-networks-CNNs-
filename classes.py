b530196eecc10e70c901f9be61285666a016a57a
Mohammed <168332460+MohammedJamalDawoud@users.noreply.github.com>
2025-01-19 14:06:41 +0100

    Add class definitions for GANs and CNNs implementation

---
+++ classes.py
@@ -0,0 +1,211 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.utils import make_grid
+import torchvision.utils as vutils
+import matplotlib.animation as animation
+from IPython.display import HTML
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import pandas as pd
+import copy
+import time
+import cv2 as cv
+from tqdm import tqdm_notebook as tqdm
+import matplotlib.image as mpimg
+import numpy as np # linear algebra
+import pandas as pd
+import torchvision.transforms.functional as TF
+img_dir = 'archive/flickr30k_images/flickr30k_images/'
+img_list = os.listdir(img_dir)
+print(len(img_list))
+valid_ratio = 0.8
+batch_size=20
+
+class ImageData(Dataset):
+    def __init__(self,is_train=True):
+        self.is_train = is_train
+        self.transform = transforms.Compose([transforms.ToTensor(),])
+        self.train_index = int(valid_ratio * len(img_list))
+        self.crop = transforms.CenterCrop((218,178))
+    def __len__(self):
+        if self.is_train:
+            return self.train_index
+        else:
+            return len(img_list) - self.train_index -1
+    def __getitem__(self, index):
+        if not self.is_train:
+            index = self.train_index + index
+#         print("hey  "*4 + str(index))
+        img = mpimg.imread(img_dir+img_list[index])
+        img = self.crop(TF.to_pil_image(img))
+        img = self.transform(img)
+        img = (img-0.5) /0.5
+#         img = (img - 255.0) / 255.0
+        return img
+dataset = ImageData()
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+device = 'cuda'
+IMG_WIDTH = 178
+IMG_HEIGHT = 218
+latent_size = 200
+
+num_channels_in_encoder = 8
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+# Encoder Model
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        
+        # ENCODER
+
+        # 64x64x64
+        self.e_conv_1 = nn.Sequential(
+            nn.ZeroPad2d((1, 2, 1, 2)),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(5, 5), stride=(2, 2)),nn.LeakyReLU()
+        )
+
+        # 128x32x32
+        self.e_conv_2 = nn.Sequential(
+            nn.ZeroPad2d((1, 2, 1, 2)),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5, 5), stride=(2, 2)),
+            nn.LeakyReLU()
+        )
+        
+        # 128x32x32
+        self.e_block_1 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x32x32
+        self.e_block_2 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x32x32
+        self.e_block_3 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 32x32x32
+        self.e_conv_3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=num_channels_in_encoder, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
+            nn.Tanh()
+        )
+    def forward(self, x):
+        ec1 = self.e_conv_1(x)
+        ec2 = self.e_conv_2(ec1)
+        eblock1 = self.e_block_1(ec2) + ec2
+        eblock2 = self.e_block_2(eblock1) + eblock1
+        eblock3 = self.e_block_3(eblock2) + eblock2
+        ec3 = self.e_conv_3(eblock3)  # in [-1, 1] from tanh activation
+        return ec3
+# Generator / Decoder Model
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        
+        # DECODER
+#         self.latent_fc1 = nn.Sequential(
+#             nn.Linear(latent_size,1000),
+#             nn.Sigmoid(),
+#         )
+#         self.latent_fc2 = nn.Sequential(
+#             nn.Linear(1000,54*44),
+#             nn.Sigmoid(),
+#         )
+        # 128x64x64
+        self.d_up_conv_1 = nn.Sequential(
+        nn.Conv2d(in_channels=num_channels_in_encoder, out_channels=64, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=(2, 2), stride=(2, 2))
+        )
+
+        # 128x64x64
+        self.d_block_1 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x64x64
+        self.d_block_2 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x64x64
+        self.d_block_3 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 256x128x128
+        self.d_up_conv_2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.ConvTranspose2d(in_channels=32, out_channels=256, kernel_size=(2, 2), stride=(2, 2))
+        )
+
+        # 3x128x128
+        self.d_up_conv_3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=16, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ReflectionPad2d((3, 3, 3, 3)),
+            nn.Conv2d(in_channels=16, out_channels=3, kernel_size=(3, 3), stride=(1, 1)),
+            nn.Tanh()
+        )
+
+        
+        
+    def forward(self, x):
+        uc1 = self.d_up_conv_1(x)
+        dblock1 = self.d_block_1(uc1) + uc1
+        dblock2 = self.d_block_2(dblock1) + dblock1
+        dblock3 = self.d_block_3(dblock2) + dblock2
+        uc2 = self.d_up_conv_2(dblock3)
+        dec = self.d_up_conv_3(uc2)
+        return dec    
